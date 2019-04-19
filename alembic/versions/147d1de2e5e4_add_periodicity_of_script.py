a1937be903f0a728fa85c94a062c26153ea8ab0a
e-henry <e-henry@users.noreply.github.com>
2019-04-19 19:21:09 +0200

    Upgrade db with alembic

---
+++ alembic/versions/147d1de2e5e4_add_periodicity_of_script.py
@@ -0,0 +1,24 @@
+"""Add periodicity of script
+
+Revision ID: 147d1de2e5e4
+Revises: 
+Create Date: 2019-04-19 18:48:33.526449
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '147d1de2e5e4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('script', sa.Column('periodicity', sa.String(),server_default='daily'))
+
+
+def downgrade():
+    op.drop_column('script', 'periodicity')
