888f2c96b81c470f034669cb7aabe9fe716941be
e-henry <e-henry@users.noreply.github.com>
2019-08-30 12:55:50 +0200

    Implements 'Soon' flag on main page

--- scripto/momentjs.py
+++ scripto/momentjs.py
@@ -1,5 +1,5 @@
 from jinja2 import Markup
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class momentjs(object):
@@ -37,3 +37,34 @@ class momentjs(object):
         ):
             return True
         return False
+
+    def is_soon(self, periodicity):
+        current_time = datetime.utcnow()
+        next_exec_time = self.next_time(periodicity)
+        duration = (
+            next_exec_time - current_time
+        ).total_seconds() / 60.0  # result is in minutes
+        if duration <= 0:
+            return False
+
+        if (
+            (periodicity.lower() == "hourly" and duration <= 10)
+            or (periodicity.lower() == "daily" and duration <= 60)
+            or (periodicity.lower() == "weekly" and duration <= 1080) # 18hrs
+            or (periodicity.lower() == "monthly" and duration <= 1080) # 18hrs
+        ):
+            return True
+        return False
+
+    def next_time(self, periodicity):
+        last_exec_time = datetime.strptime(
+            self.timestamp.strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S"
+        )
+        if periodicity.lower() == "hourly":
+            return last_exec_time+timedelta(hours=1)
+        if periodicity.lower() == "daily":
+            return last_exec_time+timedelta(days=1)
+        if periodicity.lower() == "weekly":
+            return last_exec_time+timedelta(days=7)
+        if periodicity.lower() == "monthly":
+            return last_exec_time+timedelta(weeks=1)
--- testdata.py
+++ testdata.py
@@ -5,9 +5,14 @@ from scripto import Script
 if __name__ == '__main__':
     pastTime=datetime.strptime(datetime.strftime(datetime.utcnow()-timedelta(hours=2), "%Y-%m-%d %H:%M:%S"),"%Y-%m-%d %H:%M:%S")
     futurTime=datetime.strptime(datetime.strftime(datetime.utcnow()+timedelta(hours=2), "%Y-%m-%d %H:%M:%S"),"%Y-%m-%d %H:%M:%S")
+    soonTime=datetime.strptime(datetime.strftime(datetime.utcnow()-timedelta(hours=23), "%Y-%m-%d %H:%M:%S"),"%Y-%m-%d %H:%M:%S")
+    soonIn10min=datetime.strptime(datetime.strftime(datetime.utcnow()-timedelta(minutes=50), "%Y-%m-%d %H:%M:%S"),"%Y-%m-%d %H:%M:%S")
+    
     db.create_all()
     db.session.add(Script("late_script.sh", True, "old-server", pastTime, "hourly"))
     db.session.add(Script("failed_backup.sh", False, "mysql-server", datetime.utcnow(), "hourly"))
     db.session.add(Script("ok_backup.sh", True, "pg-server", datetime.utcnow(), "hourly"))
     db.session.add(Script("future_script.sh", True, "future-server", futurTime, "daily"))
+    db.session.add(Script("soon_script.sh", True, "old-server", soonTime, "daily"))
+    db.session.add(Script("soon_script.sh", True, "old-server", soonIn10min, "hourly"))
     db.session.commit()
